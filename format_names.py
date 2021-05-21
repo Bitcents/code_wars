0e4c6c604419121f175a2e289a08dd0bfe274feb
Bitcents <bitcentsup@gmail.com>
2021-05-22 07:47:00 +0900

    made some corrections in format_names

--- format_names.py
+++ format_names.py
@@ -6,14 +6,11 @@ def namelist(names) -> str:
     
     if n == 0:
         return string
-    
     if n == 1:
         return names[0]['name']
-    
     for i in range(n-2):
         name = names[i]['name']
         string += f'{name}, '
-    
     string += f"{names[n-2]['name']} & {names[n-1]['name']}"
     
     return string
